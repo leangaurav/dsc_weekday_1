59eb1545b108e84954da0e591d0f49ace9d445ca
Santhosh DevOps <santhosh.suresh1@gmail.com>
2020-07-28 17:09:46 +0530

    New file to be added; added another print statement to a.py

--- Assignments/Santhosh/a.py
+++ Assignments/Santhosh/a.py
@@ -1,3 +1,5 @@
  a =10
  b = 20
  print(a+b)
+
+ print(a**2) 
---
+++ Assignments/Santhosh/b.py
@@ -0,0 +1,10 @@
+import math
+a = math.pi
+b = 256
+radius = math.sqrt(b)
+area = a * radius**2
+
+print(area)
+
+def testing(inp):
+    print("{} was received to the function".format(inp))
